858d8c377116fcf71c48714bbdd553d42853ba86
Michael Bradley <43987545+forxinrange@users.noreply.github.com>
2018-11-01 16:20:48 +0000

    v0.1.2

--- main.js
+++ main.js
@@ -2,6 +2,52 @@ const electron = require('electron')
 
 const { app, BrowserWindow } = require('electron')
 
+const { Menu } = require('electron')
+
+const ipc = require('electron').ipcRenderer
+
+var newWindow = null
+
+
+const nativeMenus = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Exit',
+                click(){
+                    app.quit()
+                }
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Editor',
+                click(){
+                    openEditorWindow()
+                }
+            }
+        ]
+    },
+    {
+        label: 'DevTools',
+        submenu: [
+            {
+                label: 'Chromium Dev Tools',
+                accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow){
+                    focusedWindow.webContents.toggleDevTools()
+                }
+            }
+        ]
+    }
+]
+
+// Main Window
+
 function createWindow(){
 
     win = new BrowserWindow({ width: 1024, height: 728})
@@ -13,14 +59,40 @@ function createWindow(){
     })
 }
 
+// Editor Window
+
+function openEditorWindow(){
+    if(newWindow){
+        newWindow.focus()
+        return
+    }
+
+    newWindow =  new BrowserWindow({
+
+        height: 728,
+        width: 1024
+
+    })
+
+    newWindow.loadFile('editor.html')
+
+    newWindow.on('closed',function(){
+        newWindow = null
+    })
+}
+
+
+// Runtime
+
+const menu = Menu.buildFromTemplate(nativeMenus)
+
+Menu.setApplicationMenu(menu)
+
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
 
     if(process.platform !== 'darwin'){
-
         app.quit()
-
     }
-
 })
