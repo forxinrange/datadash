db005db1bfd356eb06f31f471dc442c7e89bbb0e
Michael Bradley <43987545+forxinrange@users.noreply.github.com>
2018-11-06 16:50:37 +0000

    v0.1.7 MB

--- scripts/dd_edit_engine.js
+++ scripts/dd_edit_engine.js
@@ -6,8 +6,10 @@
 // Get mySQL server details from connection_details.txt
 
 function read_mySQL_connection_details(){
+    //const {dialog} = require('electron');
     var fs = require('fs');
     var details = fs.readFileSync('connection_details.txt').toString().split("\n");
+    //var details = dialog.showOpenDialog({properties: ['openFile']});
     var connectionInfo = {
         host: details[0],
         user: details[1],
@@ -22,7 +24,7 @@ function read_mySQL_connection_details(){
 function read_target_table(){
     var fs = require('fs');
     var targetTable = fs.readFileSync('connection_details.txt').toString().split("\n");
-    return targetTable[4]; // Returns speified table
+    return targetTable[4]; // Returns specified table
 }
 
 // Returns active connection ready to initiate
@@ -45,13 +47,14 @@ function query_mySQL_database(sql_query,element){
 
         dev_Build_Test(fields,rows,element);
 
-        connection.destroy(); // Kill connection
+        connection.destroy(); // Kill connection from mySQL server
     })
 }
 
 // Function to get all field names from target table and pipe them into return function
 
 function retrieve_mySQL_fields(element){
+    var header = 'FIELDS';
     var connection = establish_mySQL_connection();
     connection.connect();
     var sql_command = "SELECT * FROM " + read_target_table();
@@ -60,38 +63,48 @@ function retrieve_mySQL_fields(element){
         for(var f = 0; f < fields.length; f++){
             field_names.push(fields[f].name);
         }
-        
         // RETURN FUNCTION HERE
 
-        populate_select_elements(element,field_names);
+        populate_select_elements(element,field_names,header);
 
-        connection.destroy(); // Kill connection
+        connection.destroy(); // Kill connection from mySQL server
     })
 }
 
 // Form creator
 
-var GLOBAL_EDIT_SECTIONS = 0;
-
-function create_select_element(formID){
-    var output = "<select id='"+GLOBAL_EDIT_SECTIONS+"field'></select>";
-    var id = GLOBAL_EDIT_SECTIONS+'field';
-    retrieve_mySQL_fields(id);
-    GLOBAL_EDIT_SECTIONS++;
-    document.getElementById(formID).innerHTML += output;
-}
-
-function delete_select_element(formID){
-    GLOBAL_EDIT_SECTIONS--;
-    select_ID = GLOBAL_EDIT_SECTIONS+"field";
-    document.getElementById(select_ID).outerHTML = "";
-}
+var GLOBAL_EDIT_SECTIONS = 0; // Global counter for field ID's
 
 // Populate select boxes with array data
 
-function populate_select_elements(element, array){
+function create_select_element(element){
+    var box_values = new Array('field','value','subvalue','subvalue2','result');
+    var M_Form = GLOBAL_EDIT_SECTIONS+'masterForm';
+    var deleteButton = '<input id="'+GLOBAL_EDIT_SECTIONS+'del" type="button" value="delete" onclick="delete_form("'+M_Form+');">';
+
+
+    document.getElementById(element).innerHTML += "<form id='"+M_Form+"'>";
+    document.getElementById(element).innerHTML += "</form>";
+    //document.getElementById(element).innerHTML += deleteButton;
+
+    outputForm = document.getElementById(M_Form);
+    for(var i = 0; i < box_values.length; i++){
+        var elem = document.createElement("select");
+        elem.id=GLOBAL_EDIT_SECTIONS+box_values[i];
+        outputForm.appendChild(elem);
+    }
+    retrieve_mySQL_fields(GLOBAL_EDIT_SECTIONS+box_values[0]);
+    GLOBAL_EDIT_SECTIONS++;
+}
+
+function delete_form(element){
+    document.getElementById(element).outerHTML = "";
+}
+
+function populate_select_elements(element, array, header){
     document.getElementById(element).innerHTML = "";
     var items = document.getElementById(element);
+    array.unshift(header);
     for(var i = 0; i < array.length; i++){
         var elem = document.createElement("option");
         elem.textContent = array[i];
@@ -101,7 +114,10 @@ function populate_select_elements(element, array){
 }
 
 
+// ########## DEV TOOLS ONLY NOT FOR PROD ###########
+
 // Debugging and dev tools
+
 function devPrint(array,element){
     var devArray;
     for(var x = 0; x < array.length;x++){
@@ -113,19 +129,15 @@ function devPrint(array,element){
 function dev_Build_Test(fields,rows,element){
     var fieldArray = new Array;
     var rowArray = new Array;
-
     for(var f = 0; f < fields.length; f++){
         fieldArray.push(fields[f].name);
         for(var r = 0; r < rows.length; r++){
             rowArray.push(rows[r][fieldArray[f]]);
         }
     }
-
     var output;
     for(var i = 0; i < rowArray.length; i++){
         output += rowArray[i];
     }
-
     document.getElementById(element).innerHTML = output;
-
 }
